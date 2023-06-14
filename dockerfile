2514499e41ecf91453223877c141c4c3062d32de
rohithreddy24 <rrrachala@ucsd.edu>
2023-06-14 01:17:12 -0700

    fixed ui

--- dockerfile
+++ dockerfile
@@ -23,6 +23,7 @@ RUN conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoo
 RUN conda install -c conda-forge matplotlib
 RUN conda install click
 RUN conda install ninja
+RUN conda install pytorch-ignite
 RUN conda install streamlit
 RUN conda install -c conda-forge cudatoolkit-dev
 RUN conda install -y jupyter
--- ui.py
+++ ui.py
@@ -1,188 +1,124 @@
-import streamlit as st
-import numpy as np
-from glob import glob
-import collections
-import random
-
 import torch
-import torchvision
 from torchvision import transforms
 import torch.nn as nn
+import collections
+from config_parser import ConfigFileparser
 
-from classifier_networks import VGG, vgg_layers, CNN
+from ignite.metrics import FID, InceptionScore
+from ignite.engine import Engine
+
+from classifier_models.vgg import VGG, vgg_layers
+from classifier_models.resnet import ResNet35
+
 from GAN_models.gan_load import make_style_gan2
-import logging
+from GAN_models.dcgan import Generator
 
-GAN_weight_file = 'GAN_models/pretrained/generators/StyleGAN2/stylegan2-ffhq-config-f.pt'
-classifier_weight_file = 'classifier_weights/celebA_MultiLabels_vgg11_classifier.pt'
-classifier_network= 'VGG'
-class_count= 39
-latent_size= 512
-gan_resolution= 1024
-gan_output_channel= 3
-classifier_input_size= 256
-classifier_input_channel=3
-noise_scale= 0.1
-training_name= 'Bald_Male_Multi'
-multi_task = True
-subset_labels = [3, 19]
-device = torch.device('cuda:0')
+import streamlit as st
+import numpy as np
 
-class FCShiftPredictor(nn.Module):
-    def __init__(self,input_dim,class_dim, inner_dim, output_dim):
-        super(FCShiftPredictor, self).__init__()
-        self.fc_direction = nn.Sequential(
-            nn.Linear(input_dim+class_dim,inner_dim),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(inner_dim,inner_dim),
-            nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(inner_dim,output_dim),
-        )
+def generate_image():
+    if config.gan_model == "DCGAN":
+        z = torch.randn(1, config.latent_size, 1, 1, device=device).repeat(4, 1, 1, 1)
+        z_noise = config.noise_scale * torch.randn(4, config.latent_size, 1, 1, device=device)
+        z_perturb = z + z_noise
         
-    def forward(self, x,c):
-        x_c = torch.cat((x,c),1)
-        dir_ = self.fc_direction(x_c)
-        return dir_
+    else:
+        z = torch.randn([1,config.latent_size]).repeat(4,1).to(device)
+        z_noise = config.noise_scale * torch.randn([4, config.latent_size]).to(device)
+        z_perturb = z + z_noise
 
-def get_logger():
-    log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-    logger = logging.getLogger('DD')
-    fileHandler = logging.FileHandler("debug.log")
-    fileHandler.setFormatter(log_formatter)
-    logger .addHandler(fileHandler)
+    if config.gan_model == "DCGAN":
+        img_orig = G(z_perturb)
+    else:
+        w = G.style_gan2.get_latent(z_perturb)
+        img_orig = G.style_gan2([w] , input_is_latent=True)[0]
+    
+    if config.gan_resolution != config.classifier_input_size:
+        img_orig = resize_transform(img_orig)
 
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(log_formatter)
-    logger .addHandler(consoleHandler)
+    y_orig = classifier(img_orig)
+    y_orig = torch.sigmoid(y_orig[0]) 
+    ig = img_orig[0].permute(1, 2, 0).cpu().detach().numpy()
+    return ig, y_orig, w, z_perturb
 
-    logger.setLevel(logging.DEBUG)
+def generate_counterfactual_image(w, z_perturb, y_target):
+    if config.gan_model == "DCGAN":
+        z_perturb_tmp = z_perturb.view(config.batch_size, config.latent_size)
+        dir_pred = shift_model(z_perturb_tmp,y_target)
+    else:
+        dir_pred = shift_model(w,y_target)
+    
+    if config.gan_model == "DCGAN":
+        dir_pred = dir_pred.view(config.batch_size, config.latent_size, 1, 1)
+        img_shift = G(z_perturb + dir_pred)
+    else:
+        img_shift = G.style_gan2([w + dir_pred] , input_is_latent=True)[0]
 
-    return logger
+    if config.gan_resolution != config.classifier_input_size:
+        img_shift = resize_transform(img_shift)
+    ig = img_shift[0].permute(1, 2, 0).cpu().detach().numpy()
+    return ig
 
-def get_classifer():
-    classifier_weights = torch.load(classifier_weight_file)
+def main():
+    if count == 0:
+        generated_image = np.zeros((256,256,3))
+        counterfactual_image = np.zeros((256,256,3))
+
+    st.title("Counterfactual Image Generator")
+    
+    # Generate Image button
+    if st.button("Generate Image"):
+        generated_image, y_orig, latents, z_perturb = generate_image()
+        y_target = ~(y_orig[:,config.subset_labels]>0.5)
+        y_target = y_target.int()    
+        counterfactual_image = generate_counterfactual_image(latents, z_perturb, y_target)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Generated Image")
+        st.image(generated_image, use_column_width=True, clamp=True)
+
+    with col2:
+        st.subheader("Counterfactual Image")
+        st.image(counterfactual_image, use_column_width=True, clamp=True)
+    
+if __name__ == "__main__":
+    configfile = "config.ini"
+    config = ConfigFileparser(configfile)
+    count = 0
+
+    classifier_weights = torch.load(config.classifier_weight_file)
     if isinstance(classifier_weights, collections.OrderedDict):
-        if classifier_network == 'VGG':
-            classifier = VGG(vgg_layers,class_count)
+        if config.classifier_network == 'VGG':
+            classifier = VGG(vgg_layers, config.class_count)
+            classifier.load_state_dict(classifier_weights)
+        elif config.classifier_network == 'ResNet':
+            classifier = ResNet35()
+            classifier.resnet.load_state_dict(classifier_weights)
         else:
-            raise ValueError('Classifier architecture {} is not defined.'.format(classifier_network))
-        classifier.load_state_dict(classifier_weights)
+            raise ValueError('Classifier architecture {} is not defined.'.format(config.classifier_network))
+        
     elif isinstance(classifier_weights,dict):
         if 'model' in classifier_weights:
             classifier_weights = classifier_weights['model']
         else:
-            logger.debug('Classifier weights file includes:',classifier_weights.keys())
+            config.logger.debug('Classifier weights file includes: %s',classifier_weights.keys())
             raise ValueError('Classifier weight file has an unknown format.')
     else:
         classifier = classifier_weights
 
     classifier = classifier.eval().to(device)
-    return classifier
 
-def get_generator():
-    G = make_style_gan2(gan_resolution, GAN_weight_file)
+    if config.gan_model == "DCGAN":
+        G = Generator(config.latent_size, 64, config.gan_output_channel)
+        G = torch.load(config.gan_weight_file)
+    else:
+        G = make_style_gan2(config.gan_resolution, config.gan_weight_file)
     G.eval()
     G = G.to(device)
-    return G
 
-def get_shift_predictor():
-    weight_file_name = 'trained_weights/shift_in_{}_{}_{}_{:1.3f}.pt'.format('w' if in_W else 'z', training_name,lbl_ids,faithfulness_ratio)
-    logger.debug("%s", weight_file_name)
-    previous_weights = sorted(glob(weight_file_name))
-    logger.debug('Loading previously trained weights ...')
-    shift_model = torch.load(previous_weights[-1])
-    shift_model.eval()
-    shift_model = shift_model.to(device)
-    return shift_model
-
-
-def generate_image():
-    z = torch.randn([1,latent_size]).repeat(1,1).to(device)
-    z_noise = noise_scale * torch.randn([1,latent_size]).to(device)
-    z_perturb = z + z_noise
-    w = generator.style_gan2.get_latent(z_perturb)
-    img_orig = generator.style_gan2([w] , input_is_latent=True)[0]
-    img_orig = resize_transform(img_orig)
-    y_orig = classifier(img_orig)
-    y_orig = torch.sigmoid(y_orig[0])
-    ig = img_orig[0].permute(1, 2, 0).cpu().detach().numpy()
-    return ig, y_orig, w
-
-def generate_counterfactual_image(w, y_target):
-    dir_pred = shift_predictor(w,y_target)
-    img_shift = generator.style_gan2([w + dir_pred] , input_is_latent=True)[0]
-    ig = img_shift[0].permute(1, 2, 0).cpu().detach().numpy()
-    return ig
-
-def main():
-    # Generate the initial image
-    generated_image = np.zeros((256,256,3))
-    counterfactual_image = np.zeros((256,256,3))
-
-
-    options = ["Bald", "Male", "Smiling", "Wearing Hat", "Young"]
-    counters = ["Hairy", "Female", "Not Smiling", "Not Wearing Hat", "Old"]
-    available_options = []
-    # Main content
-    st.title("Counterfactual Image Generator")
+    shift_model = torch.load(config.shift_predictor_weight_file)
     
-    # Generate Image button
-    if st.button("Generate Image"):
-        generated_image, y_orig, latents = generate_image()
-        y_target = ~(y_orig[:,subset_labels]>0.5)
-        index1 = random.randint(0, len(y_target) - 1)
-        index2 = random.randint(0, len(y_target) - 1)
-        y_target[index1] = 1 - y_target[index1]
-        y_target[index2] = 1 - y_target[index2]
-
-        y_target = y_target.int()
-        st.subheader("Generated Image")
-        st.image(generated_image, use_column_width=True)
-        # y_target = [1, 0, 1, 0, 0]
-        # for i, tar in enumerate(y_target):
-        #     if tar:
-        #         # st.sidebar.checkbox(counters[i])
-        #         available_options.append(counters[i])
-        #     else:
-        #         # st.sidebar.checkbox(options[i])
-        #         available_options.append(options[i])
-
-        # with st.form(key="my_form"):
-        # # Create a checkbox for each option
-        #     for option in available_options:
-        #         st.checkbox(option)
-
-        #     # Get the selected options
-        #     selected_options = st.form_submit_button(label="Submit")
-
-        #     # If the user submitted the form, print the selected options
-        #     if selected_options:
-        #         for option in selected_options:
-        #             st.write(option)
-        
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        # Display the generated image
-        st.subheader("Generated Image")
-        st.image(generated_image, use_column_width=True)
-
-    with col2:
-        # Display the counterfactual image
-        st.subheader("Counterfactual Image")
-        
-        counterfactual_image = generate_counterfactual_image(latents, y_target)
-        # counterfactual_image = np.ones((256,256,3))
-        st.image(counterfactual_image, use_column_width=True)
-
-if __name__ == '__main__':
-    logger = get_logger()
-    classifier = get_classifer()
-    generator = get_generator()
-    shift_predictor = get_shift_predictor()
-    resize_transform = transforms.Resize((classifier_input_size,classifier_input_size))
-    
-    main()
+    shift_model = shift_model.to(device).eval()
+    resize_transform = transforms.Resize((config.classifier_input_size, config.classifier_input_size))
